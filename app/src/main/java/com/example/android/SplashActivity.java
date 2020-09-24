6a0395ed9eee5f33445b8156c4032748b436f3d9
DESKTOP-1547C44\1 <xxbi1@amarsoft.com>
2020-09-24 09:46:49 +0800

    添加依赖

---
+++ app/src/main/java/com/example/android/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.android;
+
+import android.app.Activity;
+
+/**
+ * Author:      xxbi
+ * Create Date: Created in 2020/9/24 9:20
+ * Update Date:
+ * Modified By:
+ * Description: 启功页
+ */
+public class SplashActivity extends Activity {
+
+
+}
